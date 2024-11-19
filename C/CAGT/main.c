f91b205c68f3a0defa17aa26b3d0e956f2dc99c5
ikadzev <ikadzev@mail.ru>
2024-11-19 13:37:12 +0700

    C: course work

--- C/CAGT/main.c
+++ C/CAGT/main.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+union float_union {
+    float fn;
+    unsigned un;
+    int in;
+};
+
+void print_repeat(char a, int i) {
+    for (int j = 0; j < i; j++) {
+        printf(&a);
+    }
+}
+
+void print_table(int* bins) {
+    printf("sign | characteristic | mantissa\n");
+    printf("%i", bins[31]);
+    printf("      ");
+    for (int i = 30; i > 22; i--) {
+        printf("%i", bins[i]);
+    }
+    printf("         ");
+    for (int i = 22; i >= 0; i--) {
+        printf("%i", bins[i]);
+    }
+}
+
 void floatBin(unsigned n) {
     int bins[32];
     int mask;
@@ -9,9 +34,7 @@ void floatBin(unsigned n) {
         mask = 1 << i;
         bins[i] = (mask & n) >> i;
     }
-    for (int j = 31; j >= 0; j--){
-        printf("%i", bins[j]);
-    }
+    print_table(bins);
 }
 
 void intBin(unsigned n) {
@@ -22,6 +45,7 @@ void intBin(unsigned n) {
         n /= 2;
         len++;
     }
+    printf("binary int = ");
     for (int j = len - 1; j >= 0; j--){
         printf("%i", bins[j]);
     }
@@ -29,11 +53,13 @@ void intBin(unsigned n) {
 
 int main() {
     unsigned int num_int;
+    printf("int: ");
     scanf("%i", &num_int);
-    float num_float;
-    scanf("%f", &num_float);
+    union float_union num;
+    printf("float: ");
+    scanf("%f", &num.fn);
     intBin(num_int);
     printf("\n");
-    floatBin(num_float);
+    floatBin(num.un);
     return 0;
 }
