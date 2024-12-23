fda96503b89de7caa3e10836bc4edf736b284fb6
ikadzev <ikadzev@mail.ru>
2024-12-23 12:29:21 +0700

    C CAGT WIP

--- C/CAGT/main.c
+++ C/CAGT/main.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-union float_union {
-    float fn;
-    unsigned un;
-    int in;
+union double_union {
+    double fn;
+    unsigned long long in;
 };
 
 void print_repeat(char a, int i) {
@@ -14,16 +13,16 @@ void print_repeat(char a, int i) {
     }
 }
 
-void print_table(int* bins) {
+void print_table(unsigned long long n) {
     printf("sign | characteristic | mantissa\n");
-    printf("%i", bins[31]);
+    printf("%lli", n << 1 >> 63);
     printf("      ");
-    for (int i = 30; i > 22; i--) {
-        printf("%i", bins[i]);
+    for (int i = 62; i > 51; i--) {
+        printf("%lli", n << (63 - i) >> 63);
     }
-    printf("         ");
-    for (int i = 22; i >= 0; i--) {
-        printf("%i", bins[i]);
+    printf("      ");
+    for (int i = 51; i >= 0; i--) {
+        printf("%lli", n << (63 - i) >> 63);
     }
     printf("\n");
     // int mantissa10 = 0;
@@ -35,33 +34,29 @@ void print_table(int* bins) {
     // printf("\n%i", mantissa10);
 }
 
-void floatBin(unsigned n) {
-    int bins[32];
-    int mask;
-    for (int i = 0; i < 32; i++) {
-        mask = 1 << i;
-        bins[i] = (mask & n) >> i;
-    }
-    printf("binary float = \n");
-    print_table(bins);
-    int chara = (int)(n << 1 >> 24);
-    unsigned int mant = (int)(n << 9 >> 9);
-    mant += 1 << 23;
-    int por = chara - 127;
-    int dot = 23 - por;
-    int int_of_float = mant >> dot;
-    float float_of_float = 0;
-    float one = 1;
-    for (int i = dot; i > 0; i--) {
-        one /= 2;
-        // printf(" %f %f", one, float_of_float);
-        if (mant << (32 - i) >> 31) {
-            float_of_float += one;
-        }
-    }
+void floatBin(unsigned long long n) {
+    printf("binary double = \n");
+    print_table(n);
+    unsigned long long chara = n << 1 >> 53;
+    unsigned long long mant = n << 12 >> 12;
+    mant += 1 << 52;
+    printf("\n%lli", chara);
+    int por = chara - 1023;
+    int dot = 52 - por;
+    
+    // int int_of_float = mant >> dot;
+    // float float_of_float = 0;
+    // float one = 1;
+    // for (int i = dot; i > 0; i--) {
+    //     one /= 2;
+    //     // printf(" %f %f", one, float_of_float);
+    //     if (mant << (32 - i) >> 31) {
+    //         float_of_float += one;
+    //     }
+    // }
 
-    printf("\n%i", int_of_float);
-    printf(" %f", float_of_float);
+    // printf("\n%i", int_of_float);
+    // printf(" %f", float_of_float);
 }
 
 void intBin(unsigned n) {
@@ -79,17 +74,17 @@ void intBin(unsigned n) {
 }
 
 int main() {
-    unsigned int num_int;
-    printf("int: ");
-    scanf("%i", &num_int);
+    // unsigned int num_int;
+    // printf("int: ");
+    // scanf("%i", &num_int);
 
-    union float_union num;
-    printf("float: ");
-    scanf("%f", &num.fn);
+    union double_union num;
+    printf("double: ");
+    scanf("%lf", &num.fn);
 
-    intBin(num_int);
-    printf("\n");
-    floatBin(num.un);
+    // intBin(num_int);
+    // printf("\n");
+    floatBin(num.in);
 
     return 0;
 }
