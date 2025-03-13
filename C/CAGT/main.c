6f37cb05e7753631402e1fadddf3f2daf62682a7
ikadzev <ikadzev@mail.ru>
2025-03-13 20:32:16 +0700

    C: CAGT DONE (again)

--- C/CAGT/main.c
+++ C/CAGT/main.c
@@ -1,90 +1,96 @@
-// Calculation and graphic task
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 
-union double_union {
-    double fn;
-    unsigned long long in;
-};
+union struc { double d; uint64_t u; };
 
-void print_repeat(char a, int i) {
-    for (int j = 0; j < i; j++) {
-        printf("%c", a);
-    }
+uint64_t sign(uint64_t n) { return n >> 63; }
+
+uint64_t chara(uint64_t n) { return (n << 1 >> 53); }
+
+uint64_t mant(uint64_t n) { 
+    uint64_t o = n << 12 >> 12;
+    o = o | 0x10000000000000;
+    return o;
 }
 
-void print_table(unsigned long long n) {
+void print_bin(uint64_t n) { for(size_t i = 0; i < 64; i++) { printf("%li", n << i >> 63); } }
+
+void print_table(uint64_t n) {
     printf("sign | characteristic | mantissa\n");
-    printf("%lli", n << 1 >> 63);
-    printf("      ");
-    for (int i = 62; i > 51; i--) {
-        printf("%lli", n << (63 - i) >> 63);
-    }
-    printf("      ");
-    for (int i = 51; i >= 0; i--) {
-        printf("%lli", n << (63 - i) >> 63);
-    }
-    printf("\n");
-    // int mantissa10 = 0;
-    // for (int i = 22; i >= 0; i--) {
-    //     if (bins[i] == 1) {
-    //         mantissa10 += 1 << i;
-    //     }
-    // }
-    // printf("\n%i", mantissa10);
+    printf("----------------------------------------------------------------------------\n");
+    printf("%li    | ", n >> 63);
+    for (size_t i = 1; i < 12; i++) { printf("%li", n << i >> 63); }
+    printf("    | ");
+    for (size_t i = 12; i < 64; i++) { printf("%li", n << i >> 63); }
+    return;
 }
 
-void floatBin(unsigned long long n) {
-    printf("binary double = \n");
-    print_table(n);
-    unsigned long long chara = n << 1 >> 53;
-    unsigned long long mant = n << 12 >> 12;
-    mant += 1 << 52;
-    printf("\n%lli", chara);
-    int por = chara - 1023;
-    int dot = 52 - por;
-    
-    // int int_of_float = mant >> dot;
-    // float float_of_float = 0;
-    // float one = 1;
-    // for (int i = dot; i > 0; i--) {
-    //     one /= 2;
-    //     // printf(" %f %f", one, float_of_float);
-    //     if (mant << (32 - i) >> 31) {
-    //         float_of_float += one;
-    //     }
-    // }
-
-    // printf("\n%i", int_of_float);
-    // printf(" %f", float_of_float);
+void print_intbin(uint64_t n) {
+    uint64_t mant_temp = mant(n);
+    int64_t order = chara(n) - 1023;
+    if (order >= 0) {
+        for (size_t i = 0; i < order + 1; i++) {
+            printf("%li", mant_temp << 11 + i >> 63);
+        }
+    }
+    else {
+        printf("0");
+    }
+    return;
 }
 
-void intBin(unsigned n) {
-    int bins[32];
-    int len = 0;
-    for (int i = 0; (i < 32) && (n != 0); i++) {
-        bins[len] = abs(n % 2);
-        n /= 2;
-        len++;
+void print_doublebin(uint64_t n) {
+    uint64_t mant_temp = mant(n);
+    uint64_t order = chara(n) - 1023;
+    for (size_t i = 0; i < 52 - order; i++) {
+        printf("%li", mant_temp << 12 + order + i >> 63);
     }
-    printf("binary int = ");
-    for (int j = len - 1; j >= 0; j--){
-        printf("%i", bins[j]);
+    return;
+}
+
+void print_intint(uint64_t n) {
+    if (sign(n)) { printf("-"); }
+    uint64_t mant_temp = mant(n);
+    uint64_t order = chara(n) - 1023;
+    uint64_t num = mant_temp >> (52 - order);
+    printf("%lu", num);
+    return;
+}
+
+void print_doubleint(uint64_t n) {
+    uint64_t order = chara(n) - 1023;
+    uint64_t frac = mant(n) << (12 + order) >> (12 + order);
+    uint64_t one = 0xFFFFFFFFFFFFFFFF;
+    uint64_t compare = one << 52 - order;
+    uint64_t reset = one >> 12 + order;
+    while (frac > 0) {
+        frac *= 10;
+        printf("%li", (frac & compare) >> 52 - order);
+        frac = frac & reset;
     }
+    return;
 }
 
 int main() {
-    // unsigned int num_int;
-    // printf("int: ");
-    // scanf("%i", &num_int);
-
-    union double_union num;
+    union struc num;
     printf("double: ");
-    scanf("%lf", &num.fn);
+    scanf("%lf", &num.d);
 
-    // intBin(num_int);
-    // printf("\n");
-    floatBin(num.in);
+    printf("binary double: ");
+    print_bin(num.u);
+    printf("\n");
+    print_table(num.u);
+    printf("\n\n | Order: %li  Corrected order: %li", chara(num.u), chara(num.u) - 1023);
 
+    printf("\n || Integer part in binary: ");
+    print_intbin(num.u);
+    printf("\n || Fractional part in binary: 0.");
+    print_doublebin(num.u);
+    
+    printf("\n ||| Number from binary double: ");
+    print_intint(num.u);
+    printf(".");
+    print_doubleint(num.u);
+    printf("\n");
     return 0;
 }
