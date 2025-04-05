5db090bbf8e344ac0e76afb11315ca5ea4a83478
ikadzev <ikadzev@mail.ru>
2025-04-05 12:46:17 +0700

    Algo: Added Lab files

---
+++ Algo/Lab2/main.py
@@ -0,0 +1,44 @@
+# class Tribios():
+#     def __init__(self, name=""):
+#         self.name = name
+#         self.isWord = False
+#         self.kids = {}
+
+#     def insert(self, key, index=0):
+#         if index > len(key): return
+#         if not key: return
+#         if len(key) - index == 0:
+#             self.isWord = True
+#             return
+#         if key[index] not in self.kids.keys():
+#             self.kids[key[index]] = Tribios(self.name + key[index])
+#         self.kids[key[index]].insert(key, index + 1)
+
+#     def seek(self, key, index=0):
+#         if index > len(key): return False
+#         if not key: return True
+#         if len(key) - index == 0:
+#             return self.isWord
+#         if key[index] not in self.kids.keys():
+#             return False
+#         self.kids[key[index]].insert(key, index + 1)
+
+#     def print(self):
+#         if self.isWord: print(self.name, end=' ')
+#         for item in self.kids.keys():
+#             self.kids[item].print()
+
+class Bios():
+    def __init__(self, value=0):
+        self.value = value
+        self.first = None
+        self.second = None
+
+    
+
+t = Tribios()
+t.insert("abc")
+t.insert("xy")
+t.insert("bcd")
+t.print()
+        
