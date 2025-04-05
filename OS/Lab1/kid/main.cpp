d70f5cdcfdd6908d4f130664976ac3e4fe29bf9d
ikadzev <ikadzev@mail.ru>
2025-04-05 12:28:10 +0700

    OS: Lab1 PASSED

--- OS/Lab1/kid/main.cpp
+++ OS/Lab1/kid/main.cpp
@@ -2,6 +2,13 @@
 #include <sys/wait.h>
 
 int main() {
-    execlp("gnome-text-editor", "gnome-text-editor", "/home/ikadzev/hotaru/proj/nstu/OS/Lab1/task.txt", NULL);
+    // execlp("gnome-text-editor", "gnome-text-editor", "/home/ikadzev/hotaru/proj/nstu/OS/Lab1/task.txt", NULL);
+    // execlp("gnome-text-editor", "gnome-text-editor", NULL);
+    execlp("gnome-calculator", "gnome-calculator", NULL);
+    // execlp("gnome-terminal", "gnome-terminal", "/home/ikadzev/hotaru/proj/nstu/CG/CAGT2/main", NULL);
+    // execlp("libreoffice", "libreoffice", "/home/ikadzev/hotaru/telegram_down/Лабораторная работа 1 (2).doc", NULL);
+    // execlp("transmission-gtk", "transmission-gtk", NULL);
+    // execlp("gnome-system-monitor", "gnome-system-monitor", NULL);
+    // execlp("/home/ikadzev/hotaru/proj/nstu/CG/CAGT2/main", "/home/ikadzev/hotaru/proj/nstu/CG/CAGT2/main", NULL);
 	return 1;
 }
