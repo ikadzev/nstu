de9fc2b44e675d813a666575e8a536b259172341
ikadzev <ikadzev@mail.ru>
2025-06-06 12:31:51 +0700

    CG2: Lab3&4 PASS

---
+++ CG2/Lab3/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace temp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Playtime Cyrillic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(627, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 51);
+            this.button1.TabIndex = 48;
+            this.button1.Text = "Загрузить .bmp файл";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Playtime Cyrillic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(571, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Файл";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 557);
+            this.pictureBox1.TabIndex = 46;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Playtime Cyrillic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(571, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 17);
+            this.label6.TabIndex = 57;
+            this.label6.Text = "Перемещение Y";
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(574, 388);
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(271, 45);
+            this.trackBar5.TabIndex = 51;
+            this.trackBar5.TickFrequency = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Playtime Cyrillic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(571, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 17);
+            this.label5.TabIndex = 58;
+            this.label5.Text = "Перемещение X";
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(574, 320);
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(271, 45);
+            this.trackBar4.TabIndex = 52;
+            this.trackBar4.TickFrequency = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Playtime Cyrillic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(571, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "Поворот Y";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(574, 252);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(271, 45);
+            this.trackBar3.TabIndex = 53;
+            this.trackBar3.TickFrequency = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Playtime Cyrillic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(571, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "Поворот X";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(574, 184);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(271, 45);
+            this.trackBar2.TabIndex = 50;
+            this.trackBar2.TickFrequency = 30;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Playtime Cyrillic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(571, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Маштаб";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(574, 105);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(271, 45);
+            this.trackBar1.TabIndex = 49;
+            this.trackBar1.TickFrequency = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 582);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Авава";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ CG2/Lab3/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace temp
+{
+    public partial class Form1 : Form
+    {
+        float scale = 1.0f;
+        float rotateX = 0.0f;
+        float rotateY = 0.0f;
+        float translateX = 0.0f;
+        float translateY = 0.0f;
+        float[,] outBuff;
+        float[,] inBuff;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            trackBar1.Minimum = 10; trackBar1.Maximum = 300; trackBar1.Value = 100; // Масштаб
+            trackBar2.Minimum = -180; trackBar2.Maximum = 180; // Поворот X
+            trackBar3.Minimum = -180; trackBar3.Maximum = 180; // Поворот Y
+            trackBar4.Minimum = -100; trackBar4.Maximum = 100; // Перемещение X
+            trackBar5.Minimum = -100; trackBar5.Maximum = 100; // Перемещение Y
+
+            trackBar1.ValueChanged += (s, e) => { UpdateTransform(); };
+            trackBar2.ValueChanged += (s, e) => { UpdateTransform(); };
+            trackBar3.ValueChanged += (s, e) => { UpdateTransform(); };
+            trackBar4.ValueChanged += (s, e) => { UpdateTransform(); };
+            trackBar5.ValueChanged += (s, e) => { UpdateTransform(); };
+            label1.Click += label1_Click;
+            label2.Click += label2_Click;
+            label3.Click += label3_Click;
+            label4.Click += label4_Click;
+            label5.Click += label5_Click;
+            label6.Click += label6_Click;
+            button1.Click += Button1_Click;
+
+            pictureBox1.BackColor = Color.White;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "BMP Image (*.bmp)|*.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                LoadBMP(ofd.FileName);
+                UpdateTransform();
+            }
+        }
+
+        private void LoadBMP(string path)
+        {
+            Bitmap bmp = new Bitmap(path);
+            inBuff = new float[bmp.Width, bmp.Height];
+
+            for (int y = 0; y < bmp.Height; y++) for (int x = 0; x < bmp.Width; x++)
+            {
+                Color pixel = bmp.GetPixel(x, y);
+                float height = (pixel.R + pixel.G + pixel.B) / 3f;
+                inBuff[x, y] = height;
+            }
+
+            int screen = (int)Math.Sqrt(bmp.Width * bmp.Width + bmp.Height * bmp.Height);
+            outBuff = new float[screen, screen];
+            int offsetX = (screen - bmp.Width) / 2;
+            int offsetY = (screen - bmp.Height) / 2;
+
+            for (int y = 0; y < bmp.Height; y++)
+                for (int x = 0; x < bmp.Width; x++)
+                    outBuff[x + offsetX, y + offsetY] = inBuff[x, y];
+            //outBuff = inBuff;
+        }
+
+        private void UpdateTransform()
+        {
+            scale = trackBar1.Value / 100f;
+            rotateX = DegreesToRadians(trackBar2.Value);
+            rotateY = DegreesToRadians(trackBar3.Value);
+            translateX = trackBar4.Value;
+            translateY = trackBar5.Value;
+
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.Clear(Color.White);
+                Draw(g);
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void Draw(Graphics g)
+        {
+            if (inBuff == null) return;
+            int pW = pictureBox1.Width; int pH = pictureBox1.Height;
+            int W = outBuff.GetLength(0); int H = outBuff.GetLength(1);
+            float pScale = (float)pW / (float)W;
+
+            int centerX = pW / 2; int centerY = pH / 2;
+            int[] topH = new int[pW];
+            int [] bottomH = new int[pW];
+
+            for (int i = 0; i < pW; i++) { topH[i] = int.MinValue; bottomH[i] = int.MaxValue; }
+
+            int zs = 0;
+            int ze = H;
+            int stepZ = 2;
+            float angleY = RadiansToDegrees(rotateY);
+            if (angleY > 90 || angleY < -90)
+            {
+                zs = H - 1;
+                ze = -1;
+                stepZ = -stepZ;
+            }
+
+            int xs = 0;
+            int xe = W;
+            int stepX = 2;
+            float angleX = RadiansToDegrees(rotateX);
+            if (angleX > 90 || angleX < -90)
+            {
+                xs = W - 1;
+                xe = -1;
+                stepX = -1;
+            }
+
+            for (int z = zs; z != ze; z += stepZ)
+            {
+                PointF? last = null;
+
+                int[] topHtemp = (int[])topH.Clone();
+                int[] bottomHtemp = (int[])bottomH.Clone();
+
+                for (int x = xs; x != xe; x += stepX)
+                {
+                    
+                    float dx = (x - W / 2.0f) * pScale;
+                    float dy = outBuff[x, z] * (float)0.5;
+                    float dz = (z - H / 2.0f) * pScale;
+
+                    Vector3 dV = new Vector3(dx, dy, dz) * 
+                        Matrix4x4.CreateScale(scale) * 
+                        Matrix4x4.CreateRotationY(rotateY) *
+                        Matrix4x4.CreateRotationX(rotateX) * 
+                        Matrix4x4.CreateTranslation(translateX, translateY, 0);
+
+                    int sX = centerX + (int)dV.X;
+                    int sY = centerY - (int)dV.Y;
+
+                    if (sX < 0 || sX >= pW) continue;
+
+                    if (sY <= topH[sX] && sY >= bottomH[sX])
+                    {
+                        last = null;
+                        continue;
+                    }
+
+                    if (last != null)
+                    {
+                        int x0 = (int)last.Value.X;
+                        int y0 = (int)last.Value.Y;
+                        int x1 = sX;
+                        int y1 = sY;
+
+                        if (Math.Abs(x1 - x0) > 1)
+                        {
+                            int steps = Math.Abs(x1 - x0);
+                            for (int i = 1; i < steps; i++)
+                            {
+                                int xi = x0 + i * (x1 - x0) / steps;
+                                int yi = y0 + i * (y1 - y0) / steps;
+                                topHtemp[xi] = Math.Max(topHtemp[xi], yi);
+                                bottomHtemp[xi] = Math.Min(bottomHtemp[xi], yi);
+                            }
+                        }
+                        g.DrawLine(Pens.Black, x0, y0, x1, y1);
+                        //g.FillRectangle(Brushes.Black, x0, y0, 2, 2);
+                    }
+                    topHtemp[sX] = Math.Max(topHtemp[sX], sY);
+                    bottomHtemp[sX] = Math.Min(bottomHtemp[sX], sY);
+                    last = new PointF(sX, sY);
+                }
+                topH = (int[])topHtemp.Clone();
+                bottomH = (int[])bottomHtemp.Clone();
+            }
+        }
+
+        private float DegreesToRadians(float degrees)
+        {
+            return (float)(Math.PI / 180.0) * degrees;
+        }
+
+        private float RadiansToDegrees(float radians)
+        {
+            return (float)(180.0 / Math.PI) * radians;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            trackBar2.Value = 0;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            trackBar3.Value = 0;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = 100;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = 100;
+            trackBar2.Value = 0;
+            trackBar3.Value = 0;
+            trackBar4.Value = 0;
+            trackBar5.Value = 0;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            trackBar4.Value = 0;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            trackBar5.Value = 0;
+        }
+    }
+
+    public struct Vector3
+    {
+        public float X, Y, Z;
+        public Vector3(float x, float y, float z) { X = x; Y = y; Z = z; }
+
+        public static Vector3 operator *(Vector3 v, Matrix4x4 m)
+        {
+            float x = m.M[0, 0] * v.X + m.M[0, 1] * v.Y + m.M[0, 2] * v.Z + m.M[0, 3] * 1;
+            float y = m.M[1, 0] * v.X + m.M[1, 1] * v.Y + m.M[1, 2] * v.Z + m.M[1, 3] * 1;
+            float z = m.M[2, 0] * v.X + m.M[2, 1] * v.Y + m.M[2, 2] * v.Z + m.M[2, 3] * 1;
+            return new Vector3(x, y, z);
+        }
+    }
+
+    public class Matrix4x4
+    {
+        public float[,] M = new float[4, 4];
+
+        public Matrix4x4()
+        {
+            for (int i = 0; i < 4; i++)
+                M[i, i] = 1;
+        }
+
+        public static Matrix4x4 CreateScale(float s)
+        {
+            var m = new Matrix4x4();
+            m.M[0, 0] = m.M[1, 1] = m.M[2, 2] = s;
+            return m;
+        }
+        public static Matrix4x4 CreateTranslation(float dx, float dy, float dz)
+        {
+            var m = new Matrix4x4();
+            m.M[0, 3] = dx;
+            m.M[1, 3] = dy;
+            m.M[2, 3] = dz;
+            return m;
+        }
+
+        public static Matrix4x4 CreateRotationX(float angle)
+        {
+            var m = new Matrix4x4();
+            float c = (float)Math.Cos(angle);
+            float s = (float)Math.Sin(angle);
+            m.M[1, 1] = c;
+            m.M[1, 2] = -s;
+            m.M[2, 1] = s;
+            m.M[2, 2] = c;
+            return m;
+        }
+
+        public static Matrix4x4 CreateRotationY(float angle)
+        {
+            var m = new Matrix4x4();
+            float c = (float)Math.Cos(angle);
+            float s = (float)Math.Sin(angle);
+            m.M[0, 0] = c;
+            m.M[0, 2] = s;
+            m.M[2, 0] = -s;
+            m.M[2, 2] = c;
+            return m;
+        }
+    }
+}
