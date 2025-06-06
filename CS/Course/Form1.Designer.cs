adbc0fa1f9ffd4ce85f97ceeef1c5b914fc69af2
ikadzev <ikadzev@mail.ru>
2025-06-06 12:30:12 +0700

    CS: Course work DONE

---
+++ CS/Course/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Course
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Playtime Cyrillic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Скриншот";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Playtime Cyrillic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(117, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "F9";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonScreenshotKey_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Playtime Cyrillic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Запись экрана";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Playtime Cyrillic", 9.25F);
+            this.button2.Location = new System.Drawing.Point(117, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "F10/F11";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonRecordKey_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Playtime Cyrillic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Включить";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(117, 9);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBoxUpdate);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Playtime Cyrillic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "FPS";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Playtime Cyrillic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Качество";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label6.Location = new System.Drawing.Point(138, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "by Ikadzev";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Playtime Cyrillic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(175, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(17, 45);
+            this.button3.TabIndex = 6;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.UpdateFPS);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(210, 155);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Скриншотина";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CS/Course/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle selectedRegion;
+        private Process ffmpegProcess;
+        private int scrnCnt = 0;
+        private int cptrCnt = 0;
+        private int fps = 30;
+        private int crf = 25;
+        private bool toggle = false;
+        private bool recording = false;
+        private Keys screenshotKey = Keys.F9;
+        private Keys recordKey = Keys.F10;
+        private bool waitingForScreenshotKey = false;
+        private bool waitingForRecordKey = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = $"{screenshotKey}";
+            button2.Text = $"{recordKey}";
+            textBox1.Text = $"{fps}";
+            textBox2.Text = $"{crf}";
+        }
+
+        private void lockButtons()
+        {
+            label1.Enabled = false;
+            label2.Enabled = false;
+            //label3.Enabled = false;
+            label4.Enabled = false;
+            label5.Enabled = false;
+            checkBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void unlockButtons()
+        {
+            label1.Enabled = true;
+            label2.Enabled = true;
+            //label3.Enabled = false;
+            label4.Enabled = true;
+            label5.Enabled = true;
+            checkBox1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (waitingForScreenshotKey)
+            {
+                waitingForScreenshotKey = false;
+                if (keyData == recordKey)
+                {
+                    MessageBox.Show("Нельзя использовать одну клавишу для двух функций!");
+                }
+                else
+                {
+                    screenshotKey = keyData;
+                }
+                button1.Text = $"{screenshotKey}";
+                return true;
+            }
+            else if (waitingForRecordKey)
+            {
+                waitingForRecordKey = false;
+                if (keyData == screenshotKey)
+                {
+                    MessageBox.Show("Нельзя использовать одну клавишу для двух функций!");
+                }
+                else
+                {
+                    recordKey = keyData;
+                }
+                button2.Text = $"{recordKey}";
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void checkBoxUpdate(object sender, EventArgs e)
+        {
+            toggle = checkBox1.Checked;
+        }
+
+        private void buttonScreenshotKey_Click(object sender, EventArgs e)
+        {
+            waitingForScreenshotKey = true;
+            button1.Text = "Клавиша...";
+            this.Focus();
+        }
+
+        private void buttonRecordKey_Click(object sender, EventArgs e)
+        {
+            waitingForRecordKey = true;
+            button2.Text = "Клавиша...";
+            this.Focus();
+        }
+
+        private void UpdateFPS(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int newFps) && newFps > 0)
+            {
+                fps = newFps;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное значение FPS (целое число больше 0).");
+                return;
+            }
+            if (int.TryParse(textBox2.Text, out int newCrf) && newCrf > 0)
+            {
+                crf = newCrf;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное значение CRF (целое число больше 0).");
+                return;
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (toggle)
+            {
+                if (e.KeyCode == screenshotKey)
+                {
+                    using (var selector = new RegionSelector())
+                    {
+                        if (selector.ShowDialog() == DialogResult.OK)
+                        {
+                            selectedRegion = selector.SelectedRegion;
+                            var screenshot = CaptureRegion(selectedRegion);
+                            screenshot.Save($"screenshot{scrnCnt}.png");
+                            MessageBox.Show($"Скриншот сохранён как screenshot{scrnCnt++}.png");
+                        }
+                    }
+                }
+                else if (e.KeyCode == recordKey && !recording)
+                {
+                    using (var selector = new RegionSelector())
+                    {
+                        if (selector.ShowDialog() == DialogResult.OK)
+                        {
+                            selectedRegion = selector.SelectedRegion;
+                            recording = true;
+                            StartRecording(selectedRegion, fps, crf);
+                            lockButtons();
+                        }
+                    }
+                }
+                else if (e.KeyCode == recordKey && recording)
+                {
+                    recording = false;
+                    StopRecording();
+                    MessageBox.Show("Запись завершена.");
+                    unlockButtons();
+                }
+            }
+        }
+
+        private Bitmap CaptureRegion(Rectangle region)
+        {
+            Bitmap bmp = new Bitmap(region.Width, region.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(region.Location, Point.Empty, region.Size);
+            }
+            return bmp;
+        }
+
+        private void StartRecording(Rectangle region, int fps, int crf)
+        {
+            string args = $"-f gdigrab " +
+                          $"-framerate {fps} " +
+                          $"-offset_x {region.X} -offset_y {region.Y} " +
+                          $"-video_size {region.Width}x{region.Height} " +
+                          $"-i desktop " +
+                          $"-vcodec libx264 " +
+                          $" -crf {crf} " +
+                          $"-preset ultrafast " + 
+                          $"capture{cptrCnt}.mp4";
+
+            ffmpegProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg.exe",
+                    Arguments = args,
+                    RedirectStandardInput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            ffmpegProcess.ErrorDataReceived += (s, e) => Debug.WriteLine(e.Data);
+            
+            if (!ffmpegProcess.Start()) 
+            {
+                MessageBox.Show("Произошла ошибка");
+                recording = false;
+            }
+            ffmpegProcess.StandardInput.WriteLine("y");
+            ffmpegProcess.BeginErrorReadLine();
+        }
+
+        private void StopRecording()
+        {
+            if (ffmpegProcess != null && !ffmpegProcess.HasExited)
+            {
+                //ffmpegProcess.Kill();
+                ffmpegProcess.StandardInput.WriteLine("q");
+                ffmpegProcess.WaitForExit();
+                //ffmpegProcess.CloseMainWindow();
+                ffmpegProcess.Dispose();
+            }
+        }
+    }
+}
+
+public class RegionSelector : Form
+{
+    public Rectangle SelectedRegion { get; private set; }
+    private Point startPoint;
+    private Point endPoint;
+    private bool selecting = false;
+
+    public RegionSelector()
+    {
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.BackColor = Color.LightGray;
+        this.Opacity = 0.5;
+        this.WindowState = FormWindowState.Maximized;
+        this.DoubleBuffered = true;
+        this.Cursor = Cursors.Cross;
+        this.TopMost = true;
+
+        this.MouseDown += (s, e) => { startPoint = e.Location; selecting = true; };
+        this.MouseMove += (s, e) => { if (selecting) { endPoint = e.Location; Invalidate(); } };
+        this.MouseUp += (s, e) =>
+        {
+            selecting = false;
+            SelectedRegion = GetRectangle(startPoint, endPoint);
+            DialogResult = DialogResult.OK;
+            Close();
+        };
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        if (selecting)
+        {
+            var rect = GetRectangle(startPoint, endPoint);
+            e.Graphics.DrawRectangle(Pens.Red, rect);
+        }
+    }
+
+    private Rectangle GetRectangle(Point p1, Point p2)
+    {
+        int x = Math.Min(p1.X, p2.X);
+        int y = Math.Min(p1.Y, p2.Y);
+        int width = Math.Abs(p1.X - p2.X);
+        int height = Math.Abs(p1.Y - p2.Y);
+        return new Rectangle(x, y, width, height);
+    }
+}
+
