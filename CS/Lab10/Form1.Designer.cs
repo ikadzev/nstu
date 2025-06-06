e3058f7423ae1aadd73c4f0352b3a22a02e562b7
ikadzev <ikadzev@mail.ru>
2025-06-06 12:29:36 +0700

    CS: Lab10 PASS

--- CS/Lab10/Form1.Designer.cs
+++ CS/Lab10/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.mozaicLabel = new System.Windows.Forms.Label();
+            this.mozaicBar = new System.Windows.Forms.TrackBar();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mozaicBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.saveButton.Location = new System.Drawing.Point(218, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(97, 26);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.executeButton.Location = new System.Drawing.Point(115, 12);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(97, 26);
+            this.executeButton.TabIndex = 0;
+            this.executeButton.Text = "Применить";
+            this.executeButton.UseVisualStyleBackColor = true;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.loadButton.Location = new System.Drawing.Point(12, 12);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(97, 26);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Загрузить";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // mozaicLabel
+            // 
+            this.mozaicLabel.AutoSize = true;
+            this.mozaicLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.mozaicLabel.Location = new System.Drawing.Point(321, 17);
+            this.mozaicLabel.Name = "mozaicLabel";
+            this.mozaicLabel.Size = new System.Drawing.Size(145, 17);
+            this.mozaicLabel.TabIndex = 1;
+            this.mozaicLabel.Text = "Количество мозайки";
+            // 
+            // mozaicBar
+            // 
+            this.mozaicBar.Location = new System.Drawing.Point(482, 12);
+            this.mozaicBar.Name = "mozaicBar";
+            this.mozaicBar.Size = new System.Drawing.Size(230, 45);
+            this.mozaicBar.TabIndex = 2;
+            this.mozaicBar.TickFrequency = 10;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(13, 45);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(702, 500);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(727, 557);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.mozaicBar);
+            this.Controls.Add(this.mozaicLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mozaicBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Label mozaicLabel;
+        private System.Windows.Forms.TrackBar mozaicBar;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- CS/Lab10/Form1.cs
+++ CS/Lab10/Form1.cs
@@ -1,20 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab10
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        private Bitmap bmp;
+
         public Form1()
         {
             InitializeComponent();
+            mozaicBar.Minimum = 0; mozaicBar.Maximum = 100;
+            loadButton.Click += buttonOpen;
+            saveButton.Click += buttonSave;
+            mozaicBar.ValueChanged += barUpdate;
+            executeButton.Click += exc;
+        }
+
+        private void barUpdate(object sender, EventArgs e)
+        {
+            if (bmp != null)
+                mozaicLabel.Text = $"Мозайка {(int)(bmp.Width * (double)(mozaicBar.Value) / 100)}x{(int)(bmp.Height * (double)(mozaicBar.Value) / 100)}";
+        }
+
+        private void exc(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                int blockWidth = (int)((double)(mozaicBar.Value) / 100 * bmp.Width);
+                int blockHeight = (int)((double)(mozaicBar.Value) / 100 * bmp.Height);
+                applyMosaic(blockWidth, blockHeight);
+            }
+        }
+
+        private void buttonOpen(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.BMP, *.JPG, *.PNG, *.ICO)| *.bmp; *.jpg; *.png; *.ico;";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                //int width = pictureBox.Width;
+                //int height = pictureBox.Height;
+                int width = image.Width;
+                int height = image.Height;
+                pictureBox.Width = width;
+                pictureBox.Height = height;
+                bmp = new Bitmap(image, width, height);
+                pictureBox.Image = bmp;
+                mozaicLabel.Text = $"Мозайка {(int)(bmp.Width * (double)(mozaicBar.Value) / 100)}x{(int)(bmp.Height * (double)(mozaicBar.Value) / 100)}";
+            }
+        }
+
+        private void buttonSave(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                bmp = pictureBox.Image as Bitmap;
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как ...";
+                savedialog.OverwritePrompt = true;
+                savedialog.CheckPathExists = true;
+                savedialog.Filter =
+                "Bitmap File(*.bmp)|*.bmp|" +
+                "JPEG File(*.jpg)|*.jpg|" +
+                "PNG File(*.png)|*.png";
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = savedialog.FileName;
+                    string strFilExtn =
+                    fileName.Remove(0, fileName.Length - 3);
+                    switch (strFilExtn)
+                    {
+                        case "bmp":
+                            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        case "jpg":
+                            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case "png":
+                            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void applyMosaic(int blockWidth, int blockHeight)
+        {
+            if (bmp == null) return;
+            if (blockWidth <= 0 || blockHeight <= 0) return;
+            Bitmap mosaicBmp = new Bitmap(bmp.Width, bmp.Height);
+
+            for (int y = 0; y < bmp.Height; y += blockHeight)
+            {
+                for (int x = 0; x < bmp.Width; x += blockWidth)
+                {
+                    int centerX = x + blockWidth / 2;
+                    int centerY = y + blockHeight / 2;
+                    if (centerX >= bmp.Width) centerX = bmp.Width - 1;
+                    if (centerY >= bmp.Height) centerY = bmp.Height - 1;
+
+                    Color centerColor = bmp.GetPixel(centerX, centerY);
+
+                    for (int j = y; j < y + blockHeight && j < bmp.Height; j++)
+                        for (int i = x; i < x + blockWidth && i < bmp.Width; i++)
+                            mosaicBmp.SetPixel(i, j, centerColor);
+                }
+            }
+
+            pictureBox.Image = mosaicBmp;
         }
     }
 }
